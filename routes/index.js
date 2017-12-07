12ad12a87dc3657e05d9a8699f463c1bf984a883
cynth <cynlee@seas.upenn.edu>
2017-12-06 19:14:00 -0500

    most popular overall / tried to add mongo stuff but that didn't work

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,44 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+require('dotenv').load();
 
 // Connect string to MySQL
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host     : '',
-  user     : '',
-  password : '',
-  database : ''
+  host     : 'songapp-db.ck7nst26eyeh.us-west-1.rds.amazonaws.com',
+  user     : 'songapp_master',
+  password : 'songapp450',
+  database : 'songapp_db'
 });
+/*****I ADDED THIS SHIT************/
+
+var MongoClient = require('mongodb').MongoClient,
+  f = require('util').format,
+  assert = require('assert'),
+  fs = require('fs');
+
+  // Read the certificate authority
+  //var ca = [fs.readFileSync(__dirname + "/ca.pem")];
+  var cert = fs.readFileSync(__dirname + "/cis450.pem");
+
+// Connection URL mongodb://{hostname}:{port}/{dbname}
+var url = 'mongodb://ec2-user@ec2-54-173-244-199.compute-1.amazonaws.com/songapp_db';
+
+// Use connect method to connect to the Server passing in
+// additional options
+MongoClient.connect(url,  {
+  server: {
+      sslValidate:true
+    , sslCert:cert
+  }
+}, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+  db.close();
+});
+/***************/
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,8 +49,32 @@ router.get('/reference', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../', 'views', 'reference.html'));
 });
 
-router.get('/insert', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../', 'views', 'insert.html'));
+router.get('/popular', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../', 'views', 'popular.html'));
+});
+
+router.get('/popular/displaySongs', function(req, res, next) {
+
+
+  var q = 'SELECT Song.title, COUNT(ListensTo.userId) FROM Song JOIN ListensTo ON Song.songID = ListensTo.songID GROUP BY Song.title ORDER BY COUNT(ListensTo.userId) DESC LIMIT 20;'
+  connection.query(q, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+        res.json(rows);
+    }
+  });
+});
+
+router.get('/popular/displayArtists', function(req, res, next) {
+
+
+  var q = 'SELECT artists.name, COUNT(ListensTo.userID) as count FROM artists JOIN PerformedBy ON artists.artistID = PerformedBy.ArtistID JOIN ListensTo ON PerformedBy.songID = ListensTo.songID GROUP BY artists.name ORDER BY COUNT(ListensTo.UserId) DESC LIMIT 20;'
+  connection.query(q, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+        res.json(rows);
+    }
+  });
 });
 
 router.get('/data/:email', function(req,res) {
@@ -33,7 +86,7 @@ router.get('/data/:email', function(req,res) {
   // you may change the query during implementation
   var email = req.params.email;
   if (email != 'undefined') query = query + ' where p.login ="' + email + '"';
-  console.log(q);
+  //console.log(q);
   var q = 'SELECT * FROM artists Limit 4;'
   connection.query(q, function(err, rows, fields) {
     if (err) console.log(err);
